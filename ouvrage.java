e21cf81c6ea3bd4e5b32f4a97f94bc6c02ce9ee4
marieamelie19 <125974477+marieamelie19@users.noreply.github.com>
2023-02-21 11:57:23 +0100

    Create ouvrage.java

---
+++ ouvrage.java
@@ -0,0 +1,15 @@
+public class ouvrage {
+
+private int numouvrage;
+private String titre;
+private String editeur;
+private String resume;
+private String disponibilite;
+
+public ouvrage (int numouvrage, String titre, String editeur, String resume, String disponibilite)
+{  numouvrage=numouvrage;
+    titre = titre;
+    editeur = editeur;
+    resume = resume;
+    disponibilite = disponibilite; }
+}
