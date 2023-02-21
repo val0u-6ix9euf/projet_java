cce16b0fd29e5d65dd39d46d2f26dda8d85af35a
val0u-6ix9euf <125970550+val0u-6ix9euf@users.noreply.github.com>
2023-02-21 11:44:22 +0100

    Create abonne.java

---
+++ abonne.java
@@ -0,0 +1,45 @@
+public class abonne extend utilisateur{
+    private int numero; 
+    private String adresse;
+    private Date dateabonnement;
+    private Date datesuspendu;
+    
+    public abonne (int numero, String adresse, Date dateabonnement, Date datesuspendu){
+    this.numero=numero; 
+    this.adresse=adresse;
+    this.dateabonnement=dateabonnement;
+    this.datesuspendu=datesuspendu;
+    }
+
+     public int getNumero() {
+        return numero;
+    }
+
+    public void setNumero(int numero) {
+        this.numero = numero;
+    }
+
+    public String getAdresse() {
+        return adresse;
+    }
+
+    public void setAdresse (String adresse) {
+        this.adresse = adresse;
+    }
+
+       public Date getDateabonnement() {
+        return dateabonnement;
+    }
+
+    public void setDateabonnement (String dateabonnement) {
+        this.dateabonnement = dateabonnement;
+    }
+
+           public Date getDatesuspebndu() {
+        return datesuspendu;
+    }
+
+    public void setDatesuspendu (String datesuspendu) {
+        this.datesuspendu = datesuspendu;
+    }
+}
