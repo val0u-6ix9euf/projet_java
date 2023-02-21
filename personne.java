b419ab7cadac08d75cc45dcecf0b09ae638c82e7
marieamelie19 <125974477+marieamelie19@users.noreply.github.com>
2023-02-21 11:58:00 +0100

    Create personne.java

---
+++ personne.java
@@ -0,0 +1,20 @@
+public class personne {
+private String nom;
+private String prenom;
+
+public personne (String nom, String prenom)
+{nom = nom
+prenom = prenom}
+}
+
+public String getNom(){
+return nom;}
+
+public void setNom (String Nom){
+    this.nom = nom ;}
+
+public String getPrenom(){
+return prenom;}
+
+public void setPrenom (String prenom){
+    this.prenom = prenom ;}
