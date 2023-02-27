9df18b15c8452a0b968f9e868e135313dba52ef2
marieamelie19 <125974477+marieamelie19@users.noreply.github.com>
2023-02-27 14:40:02 +0100

    Update pret.java

--- pret.java
+++ pret.java
@@ -1,28 +1,43 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetjava;
+
+import java.util.Date;
+
 public class pret {
     private int numpret;
     private Date dateemprunt;
     private Date dateretour;
 
-public pret (int numpret, Date dateemprunt, Date dateretour)
-{this.numpret = numpret;                                            // rajouter this
-this.dateemprunt= dateemprunt;
-this.dateretour=dateretour;}
+    public pret (int numpret, Date dateemprunt, Date dateretour) {
+        this.numpret = numpret;
+        this.dateemprunt = dateemprunt;
+        this.dateretour = dateretour;
+    }
+
+    public int getNumpret() {
+        return numpret;
+    }
+
+    public void setNumpret(int numpret) {
+        this.numpret = numpret;
+    }
+
+    public Date getDateemprunt() {
+        return dateemprunt;
+    }
+
+    public void setDateemprunt(Date dateemprunt) {
+        this.dateemprunt = dateemprunt;
+    }
+
+    public Date getDateretour() {
+        return dateretour;
+    }
+
+    public void setDateretour(Date dateretour) {
+        this.dateretour = dateretour;
+    }
 }
-
-public int getNumpret(){
-return numpret;}
-
-public void setNumpret(int numpret){
-    this.numpret = numpret ;}
-
-public Date getDateemprunt(){
-    return dateemprunt;}
-
-public void setDateemprunt (Date dateemprunt){
-    this.dateemprunt = dateemprunt;}
-
-public Date getDateretour(){
-return dateretour;}
-
-public void setDateretour(Date dateretour){
-    this.dateretour = dateretour ;}
