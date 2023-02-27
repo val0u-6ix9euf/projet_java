a41bd9429c9faab57aece666e82929e086a81c4b
marieamelie19 <125974477+marieamelie19@users.noreply.github.com>
2023-02-27 14:39:04 +0100

    Update ouvrage.java

--- ouvrage.java
+++ ouvrage.java
@@ -1,3 +1,9 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package projetjava;
+
 public class ouvrage {
 
 private int numouvrage;
@@ -6,10 +12,14 @@ private String editeur;
 private String resume;
 private String disponibilite;
 
+
 public ouvrage (int numouvrage, String titre, String editeur, String resume, String disponibilite)
 {  numouvrage=numouvrage;
     titre = titre;
     editeur = editeur;
     resume = resume;
     disponibilite = disponibilite; }
+
+// rajouter get et set
+
 }
