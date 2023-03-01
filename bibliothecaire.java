d462f9e797d8f241dddbee1384046067f7290152
marieamelie19 <125974477+marieamelie19@users.noreply.github.com>
2023-03-01 11:54:47 +0100

    Update bibliothecaire.java

--- bibliothecaire.java
+++ bibliothecaire.java
@@ -1,14 +1,10 @@
-/*
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
- */
 package projetjava;
 
-public class bibliothecaire extends utilisateur {
+public class Bibliothecaire extends Utilisateur {
     private int numero; 
     private String specialite;
 
-    public bibliothecaire (String nom, String prenom, String login, String mdp, int numero, String specialite){
+    public Bibliothecaire (String nom, String prenom, String login, String mdp, int numero, String specialite){
         super (login, mdp, nom, prenom);
         this.numero=numero; 
         this.specialite=specialite;
